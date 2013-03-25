a690e5b8e9e3f67a0e10a3a7096b15c0c14f9421
Milad Khajavi <khajavi@gmail.com>
2013-03-25 12:20:06 +0430

    Classic Strategy Pattern was added.

---
+++ Strategy/Classic/C++/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Strategy {
+public:
+	virtual void AlgorithmInterface() = 0;
+};
+
+
+class ConcreteStrategyA : public Strategy {
+public:
+	virtual void AlgorithmInterface() {
+		cout << "AlgorithmInterface implemented by ConcreteStrategyA\n" << endl;
+	}
+};
+
+
+class ConcreteStrategyB : public Strategy {
+public:
+	virtual void AlgorithmInterface() {
+		cout << "AlgorithmInterface implemented by ConcreteStrategyB\n" << endl;
+	}
+};
+
+class Context {
+public:
+	Context(Strategy* strategy)
+		: _strategy(strategy) {
+	}
+
+	void ContextInterface() {
+		if ( NULL != _strategy) {
+			_strategy->AlgorithmInterface();
+		}
+	}
+private:
+	Strategy* _strategy;
+};
+
+
+
+int main (int argc, char const* argv[]) {
+	
+	Context* context = new Context(new ConcreteStrategyA());
+	context->ContextInterface();
+
+	return 0;
+}
